35f087da857d615546ee8ab47572731147ffd6eb
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2021-08-17 02:49:55 -0700

    Add project files.

---
+++ EPM.Repository/Master/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPM.Repository.Master
+{
+    public interface IEmployeeRepository
+    {
+
+    }
+}
