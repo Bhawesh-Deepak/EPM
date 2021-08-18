da1febe9f50f6177b205b7becd3de1f4177ce7aa
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2021-08-17 20:34:32 -0700

    EPICustom Authenticate

---
+++ EPM.Core/Master/Product.cs
@@ -0,0 +1,18 @@
+﻿using EPM.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPM.Core.Master
+{
+    public class Product: BaseModel<int>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ProductImage { get; set; }
+        public string VideoPath { get; set; }
+    }
+}
